1f6428ab87d393225235130623828789d1068c8c
scottkall <scottk@hms.harvard.edu>
2016-07-25 10:42:22 -0400

    minor ctable label reversion

--- server.R
+++ server.R
@@ -205,7 +205,7 @@ shinyServer(function(input, output, session) {  #TODO: read on what 'session' me
     
     if (input$ctable_numcats == 1) {
       tmp_table <- table(ctable_df[,input$tablevar1])
-      names(dimnames(tmp_table)) <- "count"
+      names(dimnames(tmp_table)) <- "category"
       tmp_table
     } else if (input$ctable_numcats == 2) {
       as.data.frame.matrix(table(ctable_df[,input$tablevar1],ctable_df[,input$tablevar2]))
