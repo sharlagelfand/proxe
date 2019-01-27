c83ea4a77664d5f0657b0eff6e4e7e3cadad0925
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-01-27 16:24:47 -0500

    Add viral transcript detection cleaning script to list of scripts for pre-compiled data

--- compile_upload.R
+++ compile_upload.R
@@ -4,8 +4,7 @@
 rm(list=ls())
 
 # set order of read-in scripts
-read_in_scripts = c("clean_liquid.R","seq.R","rna_seq.R","oncoprint2.R",
-  "clean_solid.R","jax.R","finalize.R")
+read_in_scripts = c("clean_liquid.R", "clean_virusseq.R", "seq.R", "rna_seq.R", "oncoprint2.R", "clean_solid.R", "jax.R", "finalize.R")
 
 # set working directory based on user -- hardcoded for Mark and Scott
 host <- system("hostname",intern=TRUE)
