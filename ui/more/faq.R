cb75e4ee6d5e1b77d9cf50849647c16e75e920ee
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-01-28 20:03:38 -0500

    Reorganize 'more' UI components

---
+++ ui/more/faq.R
@@ -0,0 +1,9 @@
+ui_more_faq <- function(){
+  tabPanel("FAQ",
+           h1("Frequently Asked Questions"),
+           # TODO: ideally, eventually make a TOC to each item at the top, linking via anchors or shiny equivalent.
+           column(width = 10,
+                  uiOutput("FAQ")
+           )
+  )
+}
