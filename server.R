c2be6355024adea204af4fcb70c42598dd6ddc31
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-01-28 19:14:59 -0500

    Use log2 of FPKM? This is what other heatmaps in the app are doing

--- server.R
+++ server.R
@@ -296,8 +296,8 @@ shinyServer(function(input, output, session) {  #TODO: read on what 'session' me
   output$plot_pdx_viral_transcript_detection <- renderPlot({
     virusseq_fpkm_matrix_selected_genes <- virusseq_fpkm_matrix[rownames(virusseq_fpkm_matrix) %in% input$pdx_viral_transcript_genes, ]
     
-    heatmap.2(virusseq_fpkm_matrix_selected_genes,
-              main = "FPKM",
+    heatmap.2(log(virusseq_fpkm_matrix_selected_genes + 0.01, base = 2),
+              main = "Log2( FPKM + 0.01 )",
               notecol="black",
               density.info="none",
               trace="none",
