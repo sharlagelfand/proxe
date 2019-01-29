6ed0c37b1daf12806a7cd0a28dcfbdb4d7238c79
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-01-28 20:04:04 -0500

    Run styler on main UI file

--- ui.R
+++ ui.R
@@ -1,50 +1,55 @@
 # ui.R #
 
 # find indices of transitions from obligate visible to conditionally visible to obligate invisible columns
-  # TODO: discuss with Mark that I think we should have a fourth section, thus:
-    # 1. obligate visible from the start
-    # 2. option to click to make visible
-    # 3. option to type in box to make visible (genes)
-    # 4. never visible (should we even have these in the dataframe we read in?)
+# TODO: discuss with Mark that I think we should have a fourth section, thus:
+# 1. obligate visible from the start
+# 2. option to click to make visible
+# 3. option to type in box to make visible (genes)
+# 4. never visible (should we even have these in the dataframe we read in?)
 
 # Define the overall UI
 shinyUI(
-  navbarPage(#title="Public Repository of Xenografts",
-    title=span(img(src="PRoXe-Blue-Small.png",width="90px",
-      style="float:left; padding-right:5px; display:inline; position:relative; top:-10px"),
-      "Public Repository of Xenografts"),
-    id="mainNavBar",
-    windowTitle="PRoXe: Public Repository of Xenografts",
-    
-    navbarMenu("Liquid Tumors", # UIs are defined in ui/liquid_tumors/
-               
-               ui_liquid_tumors_database_explorer(),
-               ui_liquid_tumors_pdx_gene_expression(),
-               ui_liquid_tumors_pdx_mutations(),
-               ui_liquid_tumors_contingency_table(),
-               ui_liquid_tumors_line_report(),
-               ui_liquid_tumors_glossary(),
-               ui_liquid_tumors_methods()
-               
+  navbarPage( # title="Public Repository of Xenografts",
+    title = span(
+      img(
+        src = "PRoXe-Blue-Small.png", width = "90px",
+        style = "float:left; padding-right:5px; display:inline; position:relative; top:-10px"
+      ),
+      "Public Repository of Xenografts"
     ),
-    
-    navbarMenu("Solid Tumors",
+    id = "mainNavBar",
+    windowTitle = "PRoXe: Public Repository of Xenografts",
+
+    navbarMenu(
+      "Liquid Tumors", # UIs are defined in ui/liquid_tumors/
+
+      ui_liquid_tumors_database_explorer(),
+      ui_liquid_tumors_pdx_gene_expression(),
+      ui_liquid_tumors_pdx_mutations(),
+      ui_liquid_tumors_contingency_table(),
+      ui_liquid_tumors_line_report(),
+      ui_liquid_tumors_glossary(),
+      ui_liquid_tumors_methods()
+    ),
+
+    navbarMenu(
+      "Solid Tumors",
       # tabPanel("Solid Tumors (beta)",
-      
+
       ui_solid_tumors_database_explorer(),
       ui_solid_tumors_pdx_gene_expression(),
       ui_solid_tumors_pdx_glossary()
     ),
-    
-    navbarMenu("More",
-               
-               ui_more_line_requests_pricing(), 
-               ui_more_ilab_billing_platform(),
-               ui_more_about(),
-               ui_more_faq()
+
+    navbarMenu(
+      "More",
+
+      ui_more_line_requests_pricing(),
+      ui_more_ilab_billing_platform(),
+      ui_more_about(),
+      ui_more_faq()
     )
     # navbarPage options
-    ,position="fixed-top",collapsible=TRUE
+    , position = "fixed-top", collapsible = TRUE
   )
 )
-
