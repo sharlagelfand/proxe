9eeec78b7c7e682c7e132030ac4c58c9eacd3648
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-02-09 12:18:08 -0500

    Add skeleton for line selection

--- ui/liquid_tumors/pdx_viral_transcript_detection.R
+++ ui/liquid_tumors/pdx_viral_transcript_detection.R
@@ -7,26 +7,50 @@ ui_liquid_tumors_pdx_viral_transcript_detection <- function() {
         radioButtons(
           inputId = "pdx_viral_transcript_measure",
           label = "Select a measure",
-          choices = c("log2(FPKM)" = "log2_fpkm",
-                      "FPKM" = "fpkm",
-                      "Counts" = "counts",
-                      "log(Counts)" = "log_counts"),
+          choices = c(
+            "log2(FPKM)" = "log2_fpkm",
+            "FPKM" = "fpkm",
+            "Counts" = "counts",
+            "log(Counts)" = "log_counts"
+          ),
           selected = "log2_fpkm"
         ),
-        
+
         radioButtons(
           inputId = "pdx_viral_transcript_all_transcripts",
           label = "All transcripts? May be be useful for understanding overall distribution. Overrides whatever is below.",
           choices = c("no", "yes"),
           selected = "no"
         ),
-        
+
         selectizeInput(
           inputId = "pdx_viral_transcript_transcripts",
           label = "Select transcript",
           choices = sort(rownames(virusseq_fpkm_matrix)),
           selected = sort(rownames(virusseq_fpkm_matrix))[1:5],
           multiple = TRUE
+        ),
+
+        radioButtons(
+          inputId = "pdx_viral_transcript_selection_method",
+          label = "Line selection?",
+          choices = c(
+            "All lines" = "all",
+            "By line name" = "line_name",
+            "By WHO Classification" = "who",
+            "Lines selected in database explorer" = "database_explorer"
+          ),
+          selected = "all"
+        ),
+
+        conditionalPanel(
+          condition = "input.pdx_viral_transcript_selection_method == 'line_name'",
+          selectInput(
+            inputId = "pdx_viral_transcript_line_name",
+            label = "Type or select a line name below to see a full report",
+            choices = colnames(virusseq_fpkm_matrix),
+            selected = NULL
+          )
         )
       ),
       mainPanel(
