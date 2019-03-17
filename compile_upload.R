10e330df74f2432070a2fdea2c63c141ea815fc7
Sharla Gelfand <sharla.gelfand@gmail.com>
2019-03-17 19:04:40 -0400

    Style file

--- compile_upload.R
+++ compile_upload.R
@@ -1,39 +1,41 @@
 ## this script is for pre-compiling data and running/uploading app
 
 # set order of read-in scripts
-read_in_scripts = c("clean_liquid.R", "clean_virusseq.R", "seq.R","rna_seq.R","oncoprint2.R",
-  "clean_solid.R","jax.R","finalize.R")
+read_in_scripts <- c(
+  "clean_liquid.R", "clean_virusseq.R", "seq.R", "rna_seq.R", "oncoprint2.R",
+  "clean_solid.R", "jax.R", "finalize.R"
+)
 
 # set working directory based on user -- hardcoded for Mark and Scott
-host <- system("hostname",intern=TRUE)
-who <- system("whoami",intern=TRUE)
-essential_dir = c("data","global.R","server.R","ui.R","www",read_in_scripts)
-data_outside_app_dir = file.path("~","Dropbox","PRoXe","data_outside_app")  # based on default Dropbox location
-if(basename(getwd()) == "PRoXe_app" & all(essential_dir %in% dir())){
+host <- system("hostname", intern = TRUE)
+who <- system("whoami", intern = TRUE)
+essential_dir <- c("data", "global.R", "server.R", "ui.R", "www", read_in_scripts)
+data_outside_app_dir <- file.path("~", "Dropbox", "PRoXe", "data_outside_app") # based on default Dropbox location
+if (basename(getwd()) == "PRoXe_app" & all(essential_dir %in% dir())) {
   print("Seem to be in PRoXe_app directory. Continuing...")
-  app_dir = getwd()
-} else if (who == "scott"){ # not a typo
-  app_dir = file.path("~","git","PRoXe_app")
-  print(paste("User is probably Scott K. Setting working directory to",app_dir))
+  app_dir <- getwd()
+} else if (who == "scott") { # not a typo
+  app_dir <- file.path("~", "git", "PRoXe_app")
+  print(paste("User is probably Scott K. Setting working directory to", app_dir))
   setwd(app_dir)
   # data_outside_app_dir = (default)
-} else if (grepl("ArmaVirumque",host)){
-  app_dir = file.path("C:","Users","Mark","git","PRoXe","PRoXe_app")
+} else if (grepl("ArmaVirumque", host)) {
+  app_dir <- file.path("C:", "Users", "Mark", "git", "PRoXe", "PRoXe_app")
   # TODO: clone the git repo here and add in gitignore files.
-  print(paste("User is Mark. Setting working directory to",app_dir))
+  print(paste("User is Mark. Setting working directory to", app_dir))
   setwd(app_dir)
-  data_outside_app_dir = file.path("C:","Users","Mark","Dropbox (Partners HealthCare)","PRoXe","data_outside_app")
-} else if (grepl("proxe.dfci.harvard.edu",host)){
-  app_dir = file.path("~","git","PRoXe_app")
+  data_outside_app_dir <- file.path("C:", "Users", "Mark", "Dropbox (Partners HealthCare)", "PRoXe", "data_outside_app")
+} else if (grepl("proxe.dfci.harvard.edu", host)) {
+  app_dir <- file.path("~", "git", "PRoXe_app")
   # TODO: clone the git repo here and add in gitignore files.
-  print(paste("On proxe.dfci.harvard.edu VM, Setting dir to",app_dir))
+  print(paste("On proxe.dfci.harvard.edu VM, Setting dir to", app_dir))
   setwd(app_dir)
-  data_outside_app_dir = file.path("~","Dropbox","PRoXe","data_outside_app")
-} else if (who  == "sharla"){
+  data_outside_app_dir <- file.path("~", "Dropbox", "PRoXe", "data_outside_app")
+} else if (who == "sharla") {
   app_dir <- file.path("~", "tcb", "proxe")
   print(paste("User is Sharla Gelfand. Setting working directory to", app_dir))
   setwd(app_dir)
-  data_outside_app_dir <- file.path("~","tcb", "proxe", "Dropbox (Partners HealthCare)", "PRoXe", "data_outside_app") 
+  data_outside_app_dir <- file.path("~", "tcb", "proxe", "Dropbox (Partners HealthCare)", "PRoXe", "data_outside_app")
 } else {
   stop("User not recognized; manually navigate to PRoXe_app directory via setwd()")
 }
@@ -41,8 +43,8 @@ if(basename(getwd()) == "PRoXe_app" & all(essential_dir %in% dir())){
 # read_in_scripts <- read_in_scripts[1:5]
 
 # run data read-in scripts
-for(script in read_in_scripts){
-  cat(paste("--- Running:",script,"---\n"))
+for (script in read_in_scripts) {
+  cat(paste("--- Running:", script, "---\n"))
   source(script)
 }
 
@@ -51,7 +53,7 @@ z <- sapply(ls(), function(x)
   object.size(get(x)))
 as.matrix(rev(sort(z))[1:10])
 
-save.image(file = "pre-compiled.RData",compress = FALSE)
+save.image(file = "pre-compiled.RData", compress = FALSE)
 
 # print size of PRoXe_app folder
 cat("Size of app:\n")
@@ -60,7 +62,7 @@ system("du -h -d 0 ../PRoXe_app")
 # -- comment or uncomment sections below as desired -- #
 
 ## 1 - deploy
-if(F){
+if (F) {
   library(rsconnect)
   # alpha app
   rsconnect::deployApp(appDir = getwd(), appName = "PRoXe_alpha", account = "proxe")
@@ -74,8 +76,8 @@ if(F){
 # options(shiny.reactlog = TRUE)
 
 ## 2 - run normally
-rm(list=ls()) # this cleans environment to mimic shinyapps.io
-runApp(launch.browser=TRUE)
-# 
+rm(list = ls()) # this cleans environment to mimic shinyapps.io
+runApp(launch.browser = TRUE)
+#
 ## 3 - run for debugging
 # runApp(display.mode = "showcase") # for debugging
